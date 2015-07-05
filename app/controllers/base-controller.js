cb615737b9830d79eec7e6509481efaa3bc04336
= <=>
2015-07-05 16:35:59 -0300

    modi

--- app/controllers/base-controller.js
+++ app/controllers/base-controller.js
@@ -1,17 +1,3 @@
 app.controller('base', function($scope, $rootScope, $state, $timeout, viewsData) {
-    $rootScope.$on("$locationChangeSuccess", function() {
-        $timeout(function() {
-            var currentState = $state.$current.name;
-            if (currentState == "") {
-                $timeout(function() {
-                    currentState = $state.$current.name;
-                    $scope.uiTheme = viewsData.barStyles[currentState];
-                    $scope.pageTitle = viewsData.pageTitle[currentState];
-                }, 500);
-            }
-            $scope.uiTheme = viewsData.barStyles[currentState];
-            $scope.pageTitle = viewsData.pageTitle[currentState];
-
-        }, 200);
-    })
+    
 });
